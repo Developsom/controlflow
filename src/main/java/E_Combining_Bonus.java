b45748fc3a3e42cd4d5305a8598aae631695d739
Developsom <100343391+Developsom@users.noreply.github.com>
2023-05-27 18:51:04 +0200

    finished

--- src/main/java/E_Combining_Bonus.java
+++ src/main/java/E_Combining_Bonus.java
@@ -12,7 +12,11 @@ public class E_Combining_Bonus {
     // Combination 1
     //
     // implementeer een forloop die alle even nummers print. Je gebruikt hiervoor ook een if statement
-
+    for(int i = 0; i < numbers.length; i++) {
+      if(numbers[i] % 2 == 0) {
+        System.out.println(numbers[i]);
+      }
+    }
 
 
     //
@@ -24,7 +28,9 @@ public class E_Combining_Bonus {
     int index = 0;
     int accumulator = 0;
     //add while
-
+    while(index < numbers.length && accumulator <= 400) {
+      accumulator += numbers[index];
+    }
     System.out.println(accumulator);
 
   }
